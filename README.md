ad66aa6854b09795649440023350baa66f27655a
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-09-12 20:46:37 -0300

    new functionalities

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 # ftx-api-rest-extended
-An extended version of the [FTX API wrapper](https://github.com/askmike/ftx-api-rest)
-
-`npm install ftx-api-rest-extended`
-
-## Usage
-You can use the `example.js` as a reference. Request a API from your [FTX account](https://ftx.com/#a=4341346)
-
-```js
-...
-const creds = {
-    'key': 'key1',
-    'secret': 'secret1',
-    'subaccount': 'subaccount1'
-}
-...
+An extended version of the [ftx-api-rest](https://github.com/askmike/ftx-api-rest) package made by [askmike](https://github.com/askmike/ftx-api-rest), I decided to write specific functions focused on spot / future trading ✨.
+```bash
+$ npm install ftx-api-rest-extended
 ```
+## Example
+```js
+const FTX = require('ftx-api-rest-extended');
 
-## Functionalities
-1. `getBalance()`
-1. `getPrice(_pair)`
-1. `fundingRate(_pair)`
-1. `convert(_from, _to, _size)`
-1. `getQuoteInfo(_id)`
-1. `acceptQuote(_id)`
+// request the API credentials on your FTX account
+const CREDS = {
+    'key': '',
+    'secret': '',
+    'subaccount': ''
+}
 
-### Todo
-1. `buy()`
-1. `sell()`
+const FTX_INSTANCE = new FTX(CREDS);
+
+async function test() {
+    // get the price of the ETH-PERP future contract
+    const { price } = await FTX_INSTANCE.getPrice('eth-perp');
+    console.log(`[i] Exchange rate: ${price}`);
+
+    // sample of a limit buy order on the ETH-PERP future contract
+    await FTX_INSTANCE.marketOrder('0.005', 'eth-perp', 'buy', 'limit', 3000);\
+    // close all limit orders
+    const closeLimitOrders = await FTX_INSTANCE.closeLimitOrders();
+    console.log(closeLimitOrders);
+}
+
+test();
+```
+## Reference
+- [FTX API Docs](https://docs.ftx.com/?javascript#rest-api)
+
+## Wanna buy me a coffee? ☕
+ETH: mitchel.eth or 0x5584A353528dfeB2Fc751Ab48b72D32726C79507   
+BTC: bc1qqfnfkrnhv903v9v38n98vvk35hgrp77ay7kwqr or 1DhGZ1kvEF1wamq3oRb4f7pPz3cMBaxKz
--- index.js
+++ index.js
@@ -116,7 +116,7 @@ class FTXService {
      */
     async fundingRate(_pair) {
         try {
-            let req = this.FTXConnection.request({
+            const req = this.FTXConnection.request({
                 method: 'GET',
                 path: '/funding_rates',
                 data: {
@@ -125,6 +125,132 @@ class FTXService {
             });
             const res = await req;
             return await res.result;
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * This will calculate the order size based on the portfolio balance
+     * @param {number} _sizeInPercentage how big the order has to be in percentage
+     * @param {string} _pair ETH-PERP or BTC-PERP
+     */
+    async calculateOrderSize(_sizeInPercentage, _pair) {
+        try {
+            const { freeCollateral } = await this.getBalance();
+            const { price } = await this.getPrice(_pair)
+            // calculate the asset amount size of the order based on the percentage given by the user
+            const size = ((_sizeInPercentage / 100) * freeCollateral) / price;
+            return size;
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * Place your market or limit order
+     * @param {number} _size size of the order
+     * @param {string} _pair the future contract name
+     * @param {string} _side buy or sell
+     * @param {string} _type market (default) or limit order
+     * @param {string} _price specify if it's a limit order
+     * @returns 
+     */
+    async createOrder(_size, _pair, _side, _type = 'market', _price = null) {
+        try {
+            const req = this.FTXConnection.request({
+                method: 'POST',
+                path: '/orders',
+                data: {
+                    market: _pair,
+                    size: _size,
+                    side: _side,
+                    type: _type,
+                    price: _price
+                }
+            });
+            const res = await req;
+            return await res.result;
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * Get all open positions
+     * @returns an array of the opened orders
+     */
+    async getOpenPositions() {
+        try {
+            const req = this.FTXConnection.request({
+                method: 'GET',
+                path: '/positions',
+                data: {
+                    showAvgPrice: true
+                }
+            });
+
+            const res = await req;
+            const onlyPositionsWithSize = res.result.filter(pos => pos.size !== 0);
+            return onlyPositionsWithSize;
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * Get all open limit orders
+     * @returns an array of the limit orders
+     */
+    async getLimitOrders() {
+        try {
+            const req = this.FTXConnection.request({
+                method: 'GET',
+                path: '/orders'
+            });
+
+            const res = await req;
+            return res.result;
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * Close all orders or of a specific pair
+     * @param {string} _pair orders of the specific asset to close
+     */
+    async closeOrders(_pair = '') {
+        try {
+            const positions = await this.getOpenPositions();
+            if (_pair) positions = positions.filter(position => position.future.toLowerCase().includes(_pair.toLowerCase()))
+
+            if (positions.length > 0) {
+                positions.map(position => this.createOrder(position.size, position.future, position.side === 'buy' ? 'sell' : 'buy'))
+            } else {
+                console.log('No open orders');
+            }
+        } catch (e) {
+            console.error(`Something went wrong ${e}`);
+        }
+    }
+
+    /**
+     * Close all limit orders or of a specific pair
+     * @param {string} _pair orders of the specific asset to close
+     * @returns 
+     */
+    async closeLimitOrders(_pair = '') {
+        try {
+            const request = {
+                method: 'DELETE',
+                path: '/orders'
+            }
+            if (_pair) request.data = { market: _pair }
+
+            const req = this.FTXConnection.request(request);
+            const res = await req;
+            return res.result;
         } catch (e) {
             console.error(`Something went wrong ${e}`);
         }
